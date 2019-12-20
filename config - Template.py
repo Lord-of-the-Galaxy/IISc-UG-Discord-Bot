f9e23bb59c4e83a62122c2b2487e17a6029d7a8f
Lord of Galaxy <Lord-of-the-Galaxy@users.noreply.github.com>
2019-12-20 20:38:14 +0530

    Added template for config.py

---
+++ config - Template.py
@@ -0,0 +1,14 @@
+member_role_id = 000000000000000000
+senior_role_id = 111111111111111111
+mod_role_id = 222222222222222222
+muted_id = 333333333333333333
+
+welcome_id = 444444444444444444
+
+guild_id = 555555555555555555
+
+token = "<your bot token>"
+
+DB_PATH = "./data/bot.db"
+NAMES_PATH = "./data/names.txt"
+LOG_FOLDER = "./logs"
